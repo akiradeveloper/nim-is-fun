2a77ac198d150f1a891e6b618dceee812039d2c3
Akira Hayakawa <ruby.wktk@gmail.com>
2015-02-05 22:54:16 +0900

    update
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- trait.nim
+++ trait.nim
@@ -1,12 +1,28 @@
 type I = generic x
-  x.asPtr is pointer
+  # x.asPtr is pointer
   x.size is int
 
-type B = ref object
+type B = object
   p: pointer
   size: int
-
 var b = B (p:cast[pointer](0), size:10)
-proc f(i: I) =
+type C = object
+  size: int
+var c = C (size: 11)
+
+proc g(i: I) =
   echo i.size
-f(b)
+g(b)
+proc h(a: openArray[I]) =
+  discard
+h(@[b, c])
+
+type
+  M = generic x
+    # x is SomeOrdinal
+    x * x is x.type
+
+proc f(a:M): M = a * a
+
+let x = 5
+echo f(x)
