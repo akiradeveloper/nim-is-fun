507a7bc63bedb5a04d37185f816adbe51fae3e8b
Akira Hayakawa <ruby.wktk@gmail.com>
2015-02-18 21:53:16 +0900

    add tmode
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- none.nim
+++ none.nim
@@ -1,14 +1,37 @@
 # great example of option patten in nimrod
 
-type None = object
+# import typetraits
+# type None = object
+# let none = None()
 
-let none = None()
+# type None = enum none
+#
+# proc f(x: int|None): string =
+#   when x is None:
+#     result = "none!"
+#   else:
+#     result = "int!"
+#
+# echo f(10)
+# echo f(none)
 
-proc f(x: int|None): string =
-  when x is None:
-    result = "none!"
+type None = typedesc[void]
+proc g(x: int|None): string =
+  when x is int:
+    result = "int!!"
   else:
-    result = "int!"
+    result = "void!!"
+echo g(10)
+echo g(void)
 
-echo f(10)
-echo f(none)
+let n = 0
+#let x = if n == 0: 10 else: void
+let x = if false: 10 else: void
+
+# type Option[T] = T|None
+# proc g[T](x: Option[T]): string =
+#   when x in int:
+#     result = "int!!"
+#   else:
+#     result = "none!!"
+# echo g(10)
---
+++ tmode.nim
@@ -0,0 +1,3 @@
+import posix
+let n = 33206'u32
+let m = n.TMode
